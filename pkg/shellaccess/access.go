da4c1492effefe7ea5b695d901471f9c89c04ad8
Tim Drysdale <timothy.d.drysdale@gmail.com>
2020-12-27 21:16:34 +0000

    Add pkg/shellaccess

---
+++ pkg/shellaccess/access.go
@@ -0,0 +1,236 @@
+// Package shellaccess provides an API for accessing shellbar.
+// Shellaccess is aware of the roles of hosts and clients in
+// establishing ssh connections, and will allocate unique
+// connections to new clients. It requires shellbar to trigger
+// the SSH host to connect AFTER the client has connected,
+// because SSH is a server-speaks-first protocol. Hence shellaccess
+// does not need to transmit the URI of the unique connection to the host
+// because shellbar will do this when the client makes its
+// websocket connection. There is no guarantee a host is connected
+// at any given time, and if it drops its management channel
+// which is connected to the base session_id, then it cannot be
+// reached. As shellbar puts a websocket wrapper around the
+// already-encrypted TCP/IP, the communication remains encrypted
+// end-to-end. For more details on SSH security properties, see
+// https://docstore.mik.ua/orelly/networking_2ndEd/ssh/ch03_01.htm
+package shellaccess
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/go-openapi/loads"
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/go-openapi/runtime/security"
+	"github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
+	"github.com/timdrysdale/relay/pkg/permission"
+	"github.com/timdrysdale/relay/pkg/shellaccess/restapi"
+	"github.com/timdrysdale/relay/pkg/shellaccess/restapi/operations"
+	"github.com/timdrysdale/relay/pkg/ttlcode"
+)
+
+// API starts the API
+// Inputs
+// @closed - channel will be closed when server shutsdown
+// @wg - waitgroup, we must wg.Done() when we are shutdown
+// @port - where to listen locally
+// @host - external FQDN of the host (for checking against tokens) e.g. https://relay-access.practable.io
+// @target - FQDN of the relay instance e.g. wss://relay.practable.io
+// @secret- HMAC shared secret which incoming tokens will be signed with
+// @cs - pointer to the CodeStore this API shares with the shellbar websocket relay
+// @options - for future backwards compatibility (no options currently available)
+func API(closed <-chan struct{}, wg *sync.WaitGroup, port int, host, secret, target string, cs *ttlcode.CodeStore, options Options) {
+
+	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//create new service API
+	api := operations.NewAccessAPI(swaggerSpec)
+	server := restapi.NewServer(api)
+
+	//parse flags
+	flag.Parse()
+
+	// set the port this service will run on
+	server.Port = port
+
+	// set the Authorizer
+	api.BearerAuth = validateHeader(secret, host)
+
+	// set the Handler
+	api.SessionHandler = operations.SessionHandlerFunc(
+		func(params operations.SessionParams, principal interface{}) middleware.Responder {
+
+			token, ok := principal.(*jwt.Token)
+			if !ok {
+				return operations.NewSessionUnauthorized().WithPayload("Token Not JWT")
+			}
+
+			// save checking for key existence individually by checking all at once
+			claims, ok := token.Claims.(*permission.Token)
+
+			if !ok {
+				return operations.NewSessionUnauthorized().WithPayload("Token Claims Incorrect Type")
+			}
+
+			if !permission.HasRequiredClaims(*claims) {
+				return operations.NewSessionUnauthorized().WithPayload("Token Missing Required Claims")
+			}
+
+			if params.SessionID == "" {
+				return operations.NewSessionUnauthorized().WithPayload("Path Missing SessionID")
+			}
+
+			if claims.Topic != params.SessionID {
+				return operations.NewSessionUnauthorized().WithPayload("SessionID Does Not Match Token")
+			}
+
+			// Now we check the scopes ....
+			// If "host" is present, then we connect to the base session
+			// If "client" is present, then we connect to a unique sub-session
+			//  Scopes are modified to be read, write
+			// If both scopes are offered, then the behaviour depends on the routing
+			// default to treating as a host
+			// unless a ConnectionID present in query e.g.
+			// &connection_id=134234234324
+			// in which case, distinguishing between host and client is irrelevant
+
+			hasClientScope := false
+			hasHostScope := false
+			hasStatsScope := false
+
+			for _, scope := range claims.Scopes {
+				if scope == "host" {
+					hasHostScope = true
+				}
+				if scope == "client" {
+					hasClientScope = true
+				}
+				if scope == "stats" {
+					hasStatsScope = true
+				}
+			}
+
+			if hasStatsScope && params.SessionID != "stats" {
+				return operations.NewSessionUnauthorized().WithPayload("Path Not Valid for Stats Scope")
+			}
+
+			if !hasStatsScope && params.SessionID == "stats" {
+				return operations.NewSessionUnauthorized().WithPayload("Path Not Valid Without Stats Scope")
+			}
+
+			if !(hasClientScope || hasHostScope) {
+				return operations.NewSessionUnauthorized().WithPayload("Missing Client or Host Scope")
+			}
+
+			sessionID := params.SessionID
+
+			// If there is no ConnectionID, then either
+			// a/ host making their management connection to base session
+			// b/ someone looking for stats
+			// c/ client requesting a unique connection
+			// If there is no connection_id, it's not a stats request, and there is no host scope, then assume c.
+
+			if params.ConnectionID == "" && params.SessionID != "stats" && !hasHostScope {
+				// Client looking for a new unique connection
+				sessionID = sessionID + "/" + uuid.New().String()
+			}
+
+			// Shellbar will take care of alerting the admin channel of
+			// the new connection for protocol timing reasons
+			// Because ssh is "server speaks first", we want to bridge
+			// to the server only when client already in place and
+			// listening. We can
+
+			pt := permission.NewToken(
+				target,
+				claims.ConnectionType,
+				sessionID,
+				[]string{"read", "write"}, // sanitise out of abundance of caution - all use cases are read+write only
+				claims.IssuedAt,
+				claims.NotBefore,
+				claims.ExpiresAt,
+			)
+
+			code := cs.SubmitToken(pt)
+
+			uri := target + "/" + claims.ConnectionType + "/" + sessionID + "?code=" + code
+
+			return operations.NewSessionOK().WithPayload(
+				&operations.SessionOKBody{
+					URI: uri,
+				})
+
+		})
+
+	go func() {
+		<-closed
+		server.Shutdown()
+	}()
+
+	//serve API
+	if err := server.Serve(); err != nil {
+		log.Fatalln(err)
+	}
+
+	wg.Done()
+
+}
+
+// ValidateHeader checks the bearer token.
+// wrap the secret so we can get it at runtime without using global
+func validateHeader(secret, host string) security.TokenAuthentication {
+
+	return func(bearerToken string) (interface{}, error) {
+		// For apiKey security syntax see https://swagger.io/docs/specification/2-0/authentication/
+		claims := &permission.Token{}
+
+		token, err := jwt.ParseWithClaims(bearerToken, claims, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method was %v", token.Header["alg"])
+			}
+			return []byte(secret), nil
+		})
+
+		if err != nil {
+			log.WithFields(log.Fields{"error": err}).Info(err.Error())
+			return nil, fmt.Errorf("error reading token was %s", err.Error())
+		}
+
+		if !token.Valid { //checks iat, nbf, exp
+			log.Info("Token invalid")
+			return nil, fmt.Errorf("token invalid")
+		}
+
+		if claims.Audience != host {
+
+			log.WithFields(log.Fields{"aud": claims.Audience, "host": host}).Info("aud does not match this host")
+			return nil, fmt.Errorf("aud %s does not match this host %s", claims.Audience, host)
+		}
+
+		// already checked but belt and braces ....
+		if claims.ExpiresAt <= time.Now().Unix() {
+			log.Info(fmt.Sprintf("Expired at %d", claims.ExpiresAt))
+			return nil, fmt.Errorf("expired at %d", claims.ExpiresAt)
+		}
+
+		return token, nil
+	}
+}
+
+func pretty(t interface{}) string {
+
+	json, err := json.MarshalIndent(t, "", "\t")
+	if err != nil {
+		return ""
+	}
+
+	return string(json)
+}
---
+++ pkg/shellaccess/access_test.go
@@ -0,0 +1,116 @@
+package shellaccess
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/phayes/freeport"
+	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/timdrysdale/relay/pkg/permission"
+	"github.com/timdrysdale/relay/pkg/shellaccess/restapi/operations"
+	"github.com/timdrysdale/relay/pkg/ttlcode"
+)
+
+func TestGetPrefixFromPath(t *testing.T) {
+
+	assert.Equal(t, "foo%20bar", getPrefixFromPath("/foo%20bar/glum"))
+	assert.Equal(t, "", getPrefixFromPath("ooops/foo%20bar/glum"))
+
+}
+
+func TestAPI(t *testing.T) {
+
+	debug := true
+
+	if debug {
+		log.SetLevel(log.TraceLevel)
+		log.SetFormatter(&logrus.TextFormatter{FullTimestamp: true, DisableColors: true})
+		defer log.SetOutput(os.Stdout)
+
+	} else {
+		var ignore bytes.Buffer
+		logignore := bufio.NewWriter(&ignore)
+		log.SetOutput(logignore)
+	}
+
+	closed := make(chan struct{})
+	var wg sync.WaitGroup
+
+	port, err := freeport.GetFreePort()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	secret := "testsecret"
+
+	audience := "http://[::]:" + strconv.Itoa(port)
+	cs := ttlcode.NewDefaultCodeStore()
+	target := "wss://relay.example.io"
+
+	wg.Add(1)
+
+	go API(closed, &wg, port, audience, secret, target, cs, Options{})
+
+	time.Sleep(100 * time.Millisecond)
+
+	client := &http.Client{}
+
+	// Start tests
+	req, err := http.NewRequest("POST", audience+"/session/123", nil)
+
+	resp, err := client.Do(req)
+	assert.NoError(t, err)
+	body, _ := ioutil.ReadAll(resp.Body)
+	bodyStr := string([]byte(body))
+	assert.Equal(t, `{"code":401,"message":"unauthenticated for invalid credentials"}`, bodyStr)
+
+	var claims permission.Token
+
+	claims.IssuedAt = time.Now().Unix() - 1
+	claims.NotBefore = time.Now().Unix() - 1
+	claims.ExpiresAt = time.Now().Unix() + 5
+	claims.Audience = audience
+	claims.Topic = "123"
+	claims.ConnectionType = "session"
+	claims.Scopes = []string{"read", "write"}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Sign and get the complete encoded token as a string using the secret
+	bearer, err := token.SignedString([]byte(secret))
+	assert.NoError(t, err)
+
+	req, err = http.NewRequest("POST", audience+"/session/123", nil)
+	req.Header.Add("Authorization", bearer)
+
+	resp, err = client.Do(req)
+	assert.NoError(t, err)
+	body, _ = ioutil.ReadAll(resp.Body)
+
+	var p operations.SessionOKBody
+	err = json.Unmarshal(body, &p)
+	assert.NoError(t, err)
+
+	t.Log(p.URI)
+	t.Log(len("wss://relay.example.io/session/123?code="))
+
+	expected := "wss://relay.example.io/session/123?code="
+
+	assert.Equal(t, expected, p.URI[0:len(expected)])
+
+	// End tests
+	close(closed)
+	wg.Wait()
+
+}
