89a691fdd1350313f41c4b86d60fc10d175d3932
Tim Drysdale <timothy.d.drysdale@gmail.com>
2021-01-13 03:09:19 +0000

    Remove own logfile from cmd

--- cmd/shell/cmd/host.go
+++ cmd/shell/cmd/host.go
@@ -66,13 +66,6 @@ shell host
 			//production environment
 			log.SetFormatter(&log.JSONFormatter{})
 			log.SetLevel(log.WarnLevel)
-
-			file, err := os.OpenFile("shellhost.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-			if err == nil {
-				log.SetOutput(file)
-			} else {
-				log.Info("Failed to log to file, using default stderr")
-			}
 		}
 
 		// check inputs
--- cmd/shell/cmd/relay.go
+++ cmd/shell/cmd/relay.go
@@ -85,12 +85,6 @@ websocket connections are reverse proxied to the correct instance).
 			log.SetFormatter(&log.JSONFormatter{})
 			log.SetLevel(log.WarnLevel)
 
-			file, err := os.OpenFile("shellrelay.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-			if err == nil {
-				log.SetOutput(file)
-			} else {
-				log.Info("Failed to log to file, using default stderr")
-			}
 		}
 
 		// check inputs
