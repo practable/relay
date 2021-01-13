bf11239327e466d49c50ba7c3cfe47c153a7cf08
Tim Drysdale <timothy.d.drysdale@gmail.com>
2021-01-13 03:11:25 +0000

    Add build/server.sh

---
+++ build/server.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+cd ../cmd/book
+go build
+sudo cp book /usr/local/bin
+cd ../shell
+go build
+sudo cp shell /usr/local/bin
+cd ../session
+go build
+sudo cp session /usr/local/bin
+cd ../build
