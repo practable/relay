d24c2c24048531050ddf5bc15a24c62d306def98
Tim Drysdale <timothy.d.drysdale@gmail.com>
2022-02-22 14:22:29 +0000

    fix(login): upgrade to jwt/v4
    Former-commit-id: 58c51a33205158cfea72c728ce65ff18c8a6785b

--- pkg/login/models_test.go
+++ pkg/login/models_test.go
@@ -2,7 +2,9 @@ package login
 
 import (
 	"testing"
+	"time"
 
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -17,13 +19,13 @@ func TestNewTokenValidate(t *testing.T) {
 	exp := nbf + 10
 
 	token := NewToken(audience, groups, pools, scopes, iat, nbf, exp)
-	assert.Equal(t, audience, token.Audience)
+	assert.Equal(t, audience, token.Audience[0])
 	assert.Equal(t, groups, token.Groups)
 	assert.Equal(t, pools, token.Pools)
 	assert.Equal(t, scopes, token.Scopes)
-	assert.Equal(t, iat, token.IssuedAt)
-	assert.Equal(t, nbf, token.NotBefore)
-	assert.Equal(t, exp, token.ExpiresAt)
+	assert.Equal(t, *jwt.NewNumericDate(time.Unix(iat, 0)), *token.IssuedAt)
+	assert.Equal(t, *jwt.NewNumericDate(time.Unix(nbf, 0)), *token.NotBefore)
+	assert.Equal(t, *jwt.NewNumericDate(time.Unix(exp, 0)), *token.ExpiresAt)
 
 	assert.True(t, HasRequiredClaims(token))
 
