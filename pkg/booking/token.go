90ec0711d412ef3bde1e2d198a24a6ac65796ac5
Tim Drysdale <timothy.d.drysdale@gmail.com>
2022-02-22 12:33:46 +0000

    fix(booking): upgrade to jwt/v4

--- pkg/booking/token.go
+++ pkg/booking/token.go
@@ -1,6 +1,8 @@
 package booking
 
 import (
+	"time"
+
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/timdrysdale/relay/pkg/permission"
 )
@@ -9,10 +11,10 @@ import (
 func Token(audience, ct, topic, secret string, scopes []string, iat, nbf, exp int64) (string, error) {
 
 	var claims permission.Token
-	claims.IssuedAt = iat
-	claims.NotBefore = nbf
-	claims.ExpiresAt = exp
-	claims.Audience = audience
+	claims.IssuedAt = jwt.NewNumericDate(time.Unix(iat, 0))
+	claims.NotBefore = jwt.NewNumericDate(time.Unix(nbf, 0))
+	claims.ExpiresAt = jwt.NewNumericDate(time.Unix(exp, 0))
+	claims.Audience = jwt.ClaimStrings{audience}
 	claims.Topic = topic
 	claims.ConnectionType = ct // e.g. shell
 	claims.Scopes = scopes     // e.g. "host", "client", or "stats"
