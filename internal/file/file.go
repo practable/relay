7ea4d2eb7b690ec314eec4e780440a505d4e83e1
Tim Drysdale <timothy.d.drysdale@gmail.com>
2022-03-11 15:32:52 +0000

    feat(file): add regexp for parsing

--- internal/file/file.go
+++ internal/file/file.go
@@ -6,7 +6,49 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"regexp"
+	"time"
 )
+
+// regexp for parsing a comment line
+// if the first non-whitespace char on a line is # then it is ignored
+const m = "^\\s*\\#"
+
+var mr = regexp.MustCompile(m)
+
+// regexp for parsing a delay
+/* note you can include a 's' after the delay value for readability, but no other duration indicator is accepted
+e.g. these are valid delays
+[ 0.3 s] foo
+[ 0.3s ] foo
+[0.3s ] foo
+[ 0.3s] foo
+[0.3s] foo
+[ 0.3 ] foo
+[ 0.3 ] foo
+[ 0.3] foo
+[0.3 ] foo
+[0.3] foo
+[] bar
+
+and these are valid comments to be echoed to output (as opposed to ignored)
+
+[#] some comment
+[ #] some comment
+[# ] some comment
+[ # ] some comment
+
+*/
+
+// const d = "^\\s*\\[\\s*([0-9]*\\.*[0-9]*)\\s*[s]*\\s*\\]\\s*(.*)" // no echo command
+const d = "^\\s*\\[\\s*(\\#|[[0-9]*\\.*[0-9]*\\s*[s]*)\\s*\\]\\s*(.*)"
+
+var dr = regexp.MustCompile(d)
+
+// regexp for parsing a condition
+const c = "\\s*\\<\\'([^']*)'\\s*,\\s*([0-9]*)\\s*,\\s*([0-9]*)\\s*\\>"
+
+var cr = regexp.MustCompile(c)
 
 // Run connects to the session and handles writing to/from files
 func Run(ctx context.Context, hup chan os.Signal, session, token, logfilename, playfilename string) {
@@ -53,4 +95,17 @@ func ParseLine(line string) interface{} {
 
 type Error struct {
 	string
+}
+
+type Send struct {
+	Msg       string
+	Delay     time.Duration
+	Condition Condition
+}
+
+type Condition struct {
+	Active  bool
+	Filter  regexp.Regexp
+	Count   int
+	Timeout time.Duration
 }
