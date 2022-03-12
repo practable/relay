b9cb383b4989e537a8f68684593ce1e42e0ecf2a
Tim Drysdale <timothy.d.drysdale@gmail.com>
2022-03-12 17:38:34 +0000

    feat(file): add line parsing
    Former-commit-id: 358e61c53bc3d30289ca893383bca02b6c11a56e

--- internal/file/file_test.go
+++ internal/file/file_test.go
@@ -14,18 +14,22 @@ func TestParseByLine(t *testing.T) {
 # Non echo comment
 #- non echo comment 
 #+ echo comment
-[0.1s] {"an":"other"}`
+[0.1s] {"an":"other"}
+[] {"an":"other"}
+<'^foo\s*',5,0.3h1.5m0.1s> {"send":"foos"}
+
+`
 
 	in := strings.NewReader(s)
 
-	out := make(chan interface{}, 5) //make it buffered to avoid blocking
+	out := make(chan interface{}, 10) // buffer >= lines in s to avoid hang
 
 	err := ParseByLine(in, out)
 
 	assert.NoError(t, err)
 
 	for o := range out {
-		fmt.Printf("%+v\n", o)
+		fmt.Printf("%v\n", o)
 	}
 
 }
