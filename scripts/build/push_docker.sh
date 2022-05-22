a3828c568be272e644bf5bb368434bc0aa64bbf7
Tim Drysdale <timothy.d.drysdale@gmail.com>
2022-05-22 17:46:45 +0100

    Chore (docker): add push_docker script
    Former-commit-id: d91a2a0f27b8b0c7b6c0ab417ad6b9e132a05c76

---
+++ scripts/build/push_docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker tag relay_book:latest practable/relay:book-0.2.2
+docker tag relay_session:latest practable/relay:session-0.2.2
+docker tag relay_shell:latest practable/relay:shell-0.2.2
+echo "You probably need to do $docker login -u practable #enter password for account admin@practable.io at prompt"
+docker push practable/relay:book-0.2.2
+docker push practable/relay:session-0.2.2
+docker push practable/relay:shell-0.2.2 
