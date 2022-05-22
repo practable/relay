3f80daa0e2f3539a948f15f2c4ad1f9fa4bc0d6c
Tim Drysdale <timothy.d.drysdale@gmail.com>
2022-05-22 17:48:38 +0100

    Fix (chore): block docker push script until updated
    Former-commit-id: 4eb25ce7f373bb9946759aedf076c488fee3b8fe

--- scripts/build/push_docker.sh
+++ scripts/build/push_docker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "images for v0.2.2 have alreay been pushed - update push script!"
+exit
 docker tag relay_book:latest practable/relay:book-0.2.2
 docker tag relay_session:latest practable/relay:session-0.2.2
 docker tag relay_shell:latest practable/relay:shell-0.2.2
