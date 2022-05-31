85f5d594d070b4a60a5c9bd956be358f5da6b71a
Tim Drysdale <timothy.d.drysdale@gmail.com>
2022-05-31 15:19:44 +0100

    Fix (cmd): rename shell, session

---
+++ cmd/session-relay/cmd/host.go
@@ -0,0 +1,54 @@
+/*
+Copyright © 2021 Tim Drysdale <timothy.d.drysdale@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"github.com/practable/relay/internal/vw"
+	"github.com/spf13/cobra"
+)
+
+// hostCmd represents the host command
+var hostCmd = &cobra.Command{
+	Use:   "host",
+	Short: "Host video and data sessions",
+	Long: `Establishes a connection to the session relay, 
+listening for local tcp and websocket connections to forward, 
+according to rules submitted by to the RESTful-ish API.  There are sensible defaults
+for all parameters, and these can be overridden with environment variables. The defaults
+value are as follows:
+
+export RELAYHOST_PORT=8888
+export RELAYHOST_LOGLEVEL=PANIC
+export RELAYHOST_MAXBUFFERLENGTH=10
+export RELAYHOST_CLIENTBUFFERLENGTH=5
+export RELAYHOST_CLIENTTIMEOUTMS=1000
+export RELAYHOST_HTTPWAITMS=5000
+export RELAYHOST_HTTPSFLUSHMS=5
+export RELAYHOST_HTTPTIMEOUTMS=1000
+export RELAYHOST_CPUPROFULE=
+export RELAYHOST_API=
+
+session-relay host`,
+	Run: func(cmd *cobra.Command, args []string) {
+		vw.Stream()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(hostCmd)
+
+}
---
+++ cmd/session-relay/cmd/root.go
@@ -0,0 +1,56 @@
+/*
+Copyright © 2020 Tim Drysdale <timothy.d.drysdale@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "session",
+	Short: "Session is a set of services for relaying video and data streams over websockets",
+	Long: `Available services are 
+
+session relay := relays websockets, with jwt-token protected access via separate http API
+session host := connects to local sshd dameon (or whatever other login shell you are relaying)
+session client := listens at a local port for connections from your ssh tool
+`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+}
