c38ec7bda055c7dc133df4768bb21e26fb8834b8
Tim Drysdale <timothy.d.drysdale@gmail.com>
2023-11-06 12:24:30 +0000

    scripts (fix): remove stale examples

--- scripts/examples/docker/book/book.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --env-file=book.env -p 4000:4000 relay_book:latest
--- scripts/examples/session_host/setup_stream.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh 
-curl -X POST -H "Content-Type: application/json" -d '{"stream":"/stream/front/large","destination":"wss://video.practable.io:443/in/video1","id":"0"}' http://localhost:8888/api/destinations
-curl -X POST -H "Content-Type: application/json" -d '{"stream":"/stream/front/large","feeds":["video0","audio0"]}' http://localhost:8888/api/streams
-sleep 15s
-curl -X GET -H "Content-Type: application/json" http://localhost:8888/api/destinations/all
-curl -X DELETE "Content-Type: application/json" http://localhost:8888/api/destinations/0
