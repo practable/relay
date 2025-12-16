3b475484aa2f462db69e07a74998e5a190b4dd2b
Timothy Drysdale <timothy.d.drysdale@gmail.com>
2025-12-16 17:40:30 +0000

    internal/hub (fix): remove stats

--- internal/hub/hub.go
+++ internal/hub/hub.go
@@ -1,9 +1,7 @@
 package hub
 
 import (
-	"time"
-
-	"github.com/eclesh/welford"
+	log "github.com/sirupsen/logrus"
 )
 
 // New returns a pointer to an initiatialised Hub
@@ -13,22 +11,7 @@ func New() *Hub {
 		Register:   make(chan *Client),
 		Unregister: make(chan *Client),
 		Clients:    make(map[string]map[*Client]bool),
-		Stats: Stats{Audience: welford.New(),
-			Bytes:   welford.New(),
-			Latency: welford.New(),
-			Dt:      welford.New()},
 	}
-}
-
-// NewClientStats returns a pointer to a new initialised ClientStats
-func NewClientStats() *ClientStats {
-
-	c := &ClientStats{}
-	c.ConnectedAt = time.Now()
-
-	c.Rx = &Frames{Size: welford.New(), Dt: welford.New()}
-	c.Tx = &Frames{Size: welford.New(), Dt: welford.New()}
-	return c
 }
 
 // Run starts the hub
@@ -39,6 +22,9 @@ func (h *Hub) Run(closed chan struct{}) {
 			return
 		case client := <-h.Register:
 			if _, ok := h.Clients[client.Topic]; !ok {
+
+				log.WithField("topic", client.Topic).Debug("Creating topic in hub")
+
 				h.Clients[client.Topic] = make(map[*Client]bool)
 			}
 			h.Clients[client.Topic][client] = true
@@ -58,69 +44,6 @@ func (h *Hub) Run(closed chan struct{}) {
 					}
 				}
 			}
-		}
-	}
-}
-
-//RunWithStats starts the hub, and records statistics
-func (h *Hub) RunWithStats(closed chan struct{}) {
-	for {
-		select {
-		case <-closed:
-			return
-		case client := <-h.Register:
-			if _, ok := h.Clients[client.Topic]; !ok {
-				h.Clients[client.Topic] = make(map[*Client]bool)
-			}
-			h.Clients[client.Topic][client] = true
-		case client := <-h.Unregister:
-			// no guard needed SCC-S1033
-			delete(h.Clients[client.Topic], client)
-			//client knows it is finished, so no need to close(client.Send)
-
-		case message := <-h.Broadcast:
-
-			topic := message.Sender.Topic
-
-			// update hub statistics
-			dt := time.Since(h.Stats.Last)
-			if dt < 24*time.Hour {
-				h.Stats.Dt.Add(float64(dt.Seconds()))
-			}
-			h.Stats.Last = time.Now()
-			byteCount := float64(len(message.Data)) //reuse below
-			h.Stats.Bytes.Add(byteCount)
-			h.Stats.Audience.Add(float64(len(h.Clients[topic])))
-
-			//distribute messages
-			for client := range h.Clients[topic] {
-				if client.Name != message.Sender.Name {
-					select {
-					case client.Send <- message:
-						//update client RX statistics
-						dt := time.Since(client.Stats.Rx.Last)
-						if dt < 24*time.Hour {
-							client.Stats.Rx.Dt.Add(float64(dt.Seconds()))
-						}
-						client.Stats.Rx.Last = time.Now()
-						client.Stats.Rx.Size.Add(byteCount)
-					default:
-						//ignore
-						//log.WithField("client", client).Error("Unregistering unresponsive client")
-						//go func() { h.Unregister <- client }()
-					}
-				} else {
-					//update client TX statistics
-					dt := time.Since(client.Stats.Tx.Last)
-					if dt < 24*time.Hour {
-						client.Stats.Tx.Dt.Add(float64(dt.Seconds()))
-					}
-					client.Stats.Tx.Last = time.Now()
-					client.Stats.Tx.Size.Add(byteCount)
-				}
-			}
-			//update latency statistic for hub
-			h.Stats.Latency.Add(float64(time.Since(message.Sent).Seconds()))
 		}
 	}
 }
--- internal/hub/types.go
+++ internal/hub/types.go
@@ -2,8 +2,6 @@ package hub
 
 import (
 	"time"
-
-	"github.com/eclesh/welford"
 )
 
 // Hub maintains the set of active clients and
@@ -21,38 +19,6 @@ type Hub struct {
 
 	// Unregister requests from clients.
 	Unregister chan *Client
-
-	Stats Stats
-}
-
-// Stats represents overall statistics for the hub
-type Stats struct {
-	Started  time.Time
-	Last     time.Time
-	Audience *welford.Stats
-	Bytes    *welford.Stats
-	Latency  *welford.Stats
-	Dt       *welford.Stats
-}
-
-// Report represents statistics that we report externally
-type Report struct {
-	Started  string       `json:"started"`
-	Last     string       `json:"last"`
-	Audience WelfordStats `json:"audience"`
-	Bytes    WelfordStats `json:"bytes"`
-	Latency  WelfordStats `json:"latency"`
-	Dt       WelfordStats `json:"dt"`
-}
-
-// WelfordStats represents the statistical values we record
-type WelfordStats struct {
-	Count    uint64  `json:"count"`
-	Min      float64 `json:"min"`
-	Max      float64 `json:"max"`
-	Mean     float64 `json:"mean"`
-	Stddev   float64 `json:"stddev"`
-	Variance float64 `json:"variance"`
 }
 
 // Message represents a message that is wrapped and ready for multiplexing
@@ -66,44 +32,16 @@ type Message struct {
 
 // Client is a middleperson between the hub and whatever is sending/receiving messages on it
 type Client struct {
-	Hub   *Hub
-	Name  string       //for filtering who to send messages to
-	Send  chan Message // for outbound messages to client
-	Stats *ClientStats
-	Topic string // message broadcast scope is restricted to a single topic
-	Done  chan struct{}
-}
-
-// ClientStats represents statistics that we keep internally
-type ClientStats struct {
+	Hub         *Hub
+	Name        string       //for filtering who to send messages to
+	Send        chan Message // for outbound messages to client
+	Topic       string       // message broadcast scope is restricted to a single topic
+	Done        chan struct{}
 	ConnectedAt time.Time
-	Rx          *Frames
-	Tx          *Frames
-}
-
-// Frames represents statistics on (video) frames
-type Frames struct {
-	Last time.Time
-	Size *welford.Stats
-	Dt   *welford.Stats
 }
 
 // ClientReport represents statistics that we report externally
 type ClientReport struct {
-	Topic     string     `json:"topic"`
-	Connected string     `json:"connected"`
-	Stats     ClientRxTx `json:"stats"`
-}
-
-// ClientRxTx represents statistics on a particular client's communications
-type ClientRxTx struct {
-	Tx ChannelStats `json:"tx"`
-	Rx ChannelStats `json:"rx"`
-}
-
-// ChannelStats represents statistics on a particular channel's communications
-type ChannelStats struct {
-	Last  string  `json:"last"` //how many seconds ago...
-	Bytes float64 `json:"bytes"`
-	Dt    float64 `json:"dt"` //Messages per second
+	Topic       string `json:"topic"`
+	ConnectedAt string `json:"connected"`
 }
