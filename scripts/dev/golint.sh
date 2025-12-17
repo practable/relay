6b21e623c6d5192781525e781b54188e05cb0e8b
Timothy Drysdale <timothy.d.drysdale@gmail.com>
2025-12-17 11:55:24 +0000

    scripts/build (fix): lint with golangci-lint

--- scripts/dev/golint.sh
+++ scripts/dev/golint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 echo "Run this from repo root using ./scripts/dev/golint.sh"
-cd ./internal
-for d in */ ; do
-    echo "---internal/${d}---"
-    golint ${d}
-done
+golangci-lint run
+
